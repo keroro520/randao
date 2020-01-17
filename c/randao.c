175d3591d7729ffac3718ff8d9825bcee46c097b
keroro520 <keroroxx520@gmail.com>
2020-01-17 11:11:40 +0800

    chore: adjust func position

--- c/randao.c
+++ c/randao.c
@@ -8,10 +8,6 @@
 // TODO MolReader_Verify 里应该会校验长度，其实没必要在前面还检查一次 if len == xxx
 // TODO 搞懂 "static" 关键字的用处
 
-//    - All inputs/outputs are in the same phases.
-//    - IF input.phase < MAX_PHASE THEN output.phase == input.phase + 1
-//    - IF input.phase == MAX_PHASE THEN output.phase == Unknown,
-
 #define ERROR_INVALID_PHASE               -100
 #define ERROR_INVALID_CAMPAIGN_ID         -101
 #define ERROR_INVALID_DEPOSIT_CAPACITY    -102
@@ -30,28 +26,12 @@
 #define CHALLENGE_PHASE   5
 #define FINALIZE_PHASE    6
 
-mol_seg_res_t load_current_script();
-mol_seg_res_t load_current_script_hash();
-int verify_start(size_t index, size_t source);
-int verify_commit(size_t index, size_t source);
-int verify_reveal(size_t index);
-int verify_aggregate(size_t index);
-int verify_challenge(size_t index);
-int verify_finalize(size_t index);
-int verify_campaign_id(size_t index, size_t source);
-int verify_deposit_capacity(size_t index);
-int verify_commitment(size_t index, size_t source);
-int verify_reveal_witness(size_t index);
-int extract_campaign_info(bool* is_campaign_cell, uint8_t* phase, size_t index, size_t source);
-
-typedef struct {
-  uint8_t* id;        // OutPoint
+struct {
+  uint8_t* id;          // OutPoint
   uint64_t deposit;
   uint64_t period;
   uint8_t* script_hash;
-} campaign_t;
-
-campaign_t campaign;
+} campaign;
 
 mol_seg_res_t load_current_script() {
   mol_seg_res_t script_seg_res;
@@ -92,42 +72,73 @@ mol_seg_res_t load_current_script_hash() {
   return script_hash_seg_res;
 }
 
-int verify_start(size_t index, size_t source) {
-  return verify_campaign_id(index, source);
+int extract_campaign_info(
+  bool* is_campaign_cell, uint8_t* phase, size_t index, size_t source
+) {
+    int ret;
+    unsigned char actual_script_hash[HASH_SIZE];
+    uint64_t len = HASH_SIZE;
+
+    // Check if this is a campaign cell
+    ret = ckb_load_cell_by_field(
+        actual_script_hash, &len, 0, index,
+        source, CKB_CELL_FIELD_TYPE_HASH
+    );
+    if (ret != CKB_SUCCESS) {
+      return ret;
+    }
+    if (len != HASH_SIZE) {
+      return ERROR_SYSCALL;
+    }
+    if (memcmp(actual_script_hash, campaign.script_hash, HASH_SIZE) != 0) {
+      *is_campaign_cell = false;
+      return CKB_SUCCESS;
+    }
+
+    // Get phase
+    len = 8;
+    ret = ckb_load_cell_data(
+        (unsigned char *)phase, &len, 0, index, source
+    );
+    if (ret != CKB_SUCCESS) {
+      return ret;
+    }
+    if (len != 8) {
+      return ERROR_ENCODING;
+    }
+    if (*phase <= START_PHASE || FINALIZE_PHASE < *phase) {
+      return ERROR_INVALID_PHASE;
+    }
+
+    // Now we know this cell is campaign cell,
+    // and we alreaady get the `phase` and `capacity`.
+    *is_campaign_cell = true;
+    return CKB_SUCCESS;
 }
 
-int verify_commit(size_t index, size_t source) {
-  int ret;
-  ret = verify_commitment(index, source);
-  if (ret != CKB_SUCCESS) {
-    return ret;
+int initialize() {
+  // Load current script
+  mol_seg_res_t script_seg_res = load_current_script();
+  if (script_seg_res.errno != MOL_OK) {
+    return script_seg_res.errno;
   }
 
-  return verify_deposit_capacity(index);
-}
-
-int verify_reveal(size_t index) {
-  int ret = verify_deposit_capacity(index);
-  if (ret != CKB_SUCCESS) {
-    return ret;
+  // Load current script hash
+  mol_seg_res_t script_hash_seg_res = load_current_script_hash();
+  if (script_hash_seg_res.errno != MOL_OK) {
+    return script_hash_seg_res.errno;
   }
 
-  // FIXME TODO bilibili verify commitment == hash(reveal)
-  return verify_reveal_witness(index);
-}
+  mol_seg_t args_seg = MolReader_Script_get_args(&script_seg_res.seg);
+  mol_seg_t campaign_seg = MolReader_Bytes_raw_bytes(&args_seg);
+  if (MolReader_CampaignArgs_verify(&campaign_seg, false) != MOL_OK) {
+    return ERROR_ENCODING;
+  }
 
-int verify_aggregate(size_t index) {
-  // TODO
-  return CKB_SUCCESS;
-}
-
-int verify_challenge(size_t index) {
-  // TODO
-  return CKB_SUCCESS;
-}
-
-int verify_finalize(size_t index) {
-  // TODO
+  campaign.id = (uint8_t*)(MolReader_CampaignArgs_get_id(&campaign_seg).ptr);
+  campaign.period = *(uint64_t*)(MolReader_CampaignArgs_get_period(&campaign_seg).ptr);
+  campaign.deposit = *(uint64_t*)(MolReader_CampaignArgs_get_deposit(&campaign_seg).ptr);
+  campaign.script_hash = (uint8_t*)(script_hash_seg_res.seg.ptr);
   return CKB_SUCCESS;
 }
 
@@ -227,73 +238,42 @@ int verify_reveal_witness(size_t index) {
   return CKB_SUCCESS;
 }
 
-int extract_campaign_info(
-  bool* is_campaign_cell, uint8_t* phase, size_t index, size_t source
-) {
-    int ret;
-    unsigned char actual_script_hash[HASH_SIZE];
-    uint64_t len = HASH_SIZE;
-
-    // Check if this is a campaign cell
-    ret = ckb_load_cell_by_field(
-        actual_script_hash, &len, 0, index,
-        source, CKB_CELL_FIELD_TYPE_HASH
-    );
-    if (ret != CKB_SUCCESS) {
-      return ret;
-    }
-    if (len != HASH_SIZE) {
-      return ERROR_SYSCALL;
-    }
-    if (memcmp(actual_script_hash, campaign.script_hash, HASH_SIZE) != 0) {
-      *is_campaign_cell = false;
-      return CKB_SUCCESS;
-    }
-
-    // Get phase
-    len = 8;
-    ret = ckb_load_cell_data(
-        (unsigned char *)phase, &len, 0, index, source
-    );
-    if (ret != CKB_SUCCESS) {
-      return ret;
-    }
-    if (len != 8) {
-      return ERROR_ENCODING;
-    }
-    if (*phase <= START_PHASE || FINALIZE_PHASE < *phase) {
-      return ERROR_INVALID_PHASE;
-    }
-
-    // Now we know this cell is campaign cell,
-    // and we alreaady get the `phase` and `capacity`.
-    *is_campaign_cell = true;
-    return CKB_SUCCESS;
+int verify_start(size_t index, size_t source) {
+  return verify_campaign_id(index, source);
 }
 
-int initialize() {
-  // Load current script
-  mol_seg_res_t script_seg_res = load_current_script();
-  if (script_seg_res.errno != MOL_OK) {
-    return script_seg_res.errno;
+int verify_commit(size_t index, size_t source) {
+  int ret;
+  ret = verify_commitment(index, source);
+  if (ret != CKB_SUCCESS) {
+    return ret;
   }
 
-  // Load current script hash
-  mol_seg_res_t script_hash_seg_res = load_current_script_hash();
-  if (script_hash_seg_res.errno != MOL_OK) {
-    return script_hash_seg_res.errno;
+  return verify_deposit_capacity(index);
+}
+
+int verify_reveal(size_t index) {
+  int ret = verify_deposit_capacity(index);
+  if (ret != CKB_SUCCESS) {
+    return ret;
   }
 
-  mol_seg_t args_seg = MolReader_Script_get_args(&script_seg_res.seg);
-  mol_seg_t campaign_seg = MolReader_Bytes_raw_bytes(&args_seg);
-  if (MolReader_CampaignArgs_verify(&campaign_seg, false) != MOL_OK) {
-    return ERROR_ENCODING;
-  }
+  // FIXME TODO bilibili verify commitment == hash(reveal)
+  return verify_reveal_witness(index);
+}
 
-  campaign.id = (uint8_t*)(MolReader_CampaignArgs_get_id(&campaign_seg).ptr);
-  campaign.period = *(uint64_t*)(MolReader_CampaignArgs_get_period(&campaign_seg).ptr);
-  campaign.deposit = *(uint64_t*)(MolReader_CampaignArgs_get_deposit(&campaign_seg).ptr);
-  campaign.script_hash = (uint8_t*)(script_hash_seg_res.seg.ptr);
+int verify_aggregate(size_t index) {
+  // TODO
+  return CKB_SUCCESS;
+}
+
+int verify_challenge(size_t index) {
+  // TODO
+  return CKB_SUCCESS;
+}
+
+int verify_finalize(size_t index) {
+  // TODO
   return CKB_SUCCESS;
 }
 
